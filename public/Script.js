2049ed90d5724b6134d613d211e0a71a52a4b0e7
Tanni-shtha <85957434+Tanni-shtha@users.noreply.github.com>
2021-07-09 21:26:19 +0530

    Create Script.js

---
+++ public/Script.js
@@ -0,0 +1,23 @@
+  
+const videoGrid = document.getElementById('video-grid');
+console.log(videoGrid);
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+
+let MyVideoStream
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    MyVideoStream = stream;
+    addVideoStream(myVideo, stream);
+} )
+
+
+const addVideoStream = (video, stream) =>{
+    video.srcObject = stream;
+    video.addEventListener('loadmetadata', () =>{
+        video.play();
+    })
+    videoGrid.append(video);
+}
